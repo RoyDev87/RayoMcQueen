c58f0ca535bac8e3e30ee37f6bdbe809735df2a9
VelosoRaptor <re.veloso@duocuc.cl>
2023-05-13 09:52:05 -0400

    cambios

--- JS/script.js
+++ JS/script.js
@@ -12,30 +12,44 @@ checkbox.addEventListener("change", function()
 }
 );
 
-const boton_enviar = document.querySelector('#back');
-boton_enviar.addEventListener('click', () => {
-  alert('¡Hola mundo!');
-});
+console.log(document.getElementById('correo').value);
 
-const boton_limpiar = document.querySelector('#btn-entrar');
-boton_limpiar.addEventListener('click', () => {
+document.getElementById('myElement').innerHTML = document.getElementById('correo').value;
 
-  function confirmar() {
-    if (confirm("¿Está seguro de que desea continuar?")) {
-      // Código si el usuario presiona "Aceptar"
-    } else {
-      // Código si el usuario presiona "Cancelar"
-    }
-  }
+function alertCookie(){
+  console.log("Hola mundo");
+}
 
-});
+
+
+// const boton_enviar = document.querySelector('#back');
+// boton_enviar.addEventListener('click', () => {
+//   alert('¡Hola mundo!');
+// });
+
+// const boton_limpiar = document.querySelector('#btn-entrar');
+// boton_limpiar.addEventListener('click', () => {
+
+//   function confirmar() {
+//     if (confirm("¿Está seguro de que desea continuar?")) {
+//       // Código si el usuario presiona "Aceptar"
+//     } else {
+//       // Código si el usuario presiona "Cancelar"
+//     }
+//   }
+
+// });
+
+
     // EVENTOS
 
+    var x = document.cookie;
+    document.cookie = "username=René Veloso";
 
 
 
 
 
-    document.getElementById('.btn-entrar').onclick = function() {
-      window.location.href = "./index.html";
-    };
+    // document.getElementById('.btn-entrar').onclick = function() {
+    //   window.location.href = "./index.html";
+    // };
